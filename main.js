18a85b79a8d4c243f6bc214fc7989a0ff7f4d4d0
atsumini0919 <atsumini0919@gmail.com>
2023-11-16 12:58:06 +0900

    test

--- main.js
+++ main.js
@@ -26,17 +26,45 @@ var map = new maplibregl.Map({
     center: [140.74067545139434, 41.804474335038584], // 中心座標
     zoom: 13, // ズームレベル
 });
+// コントロール
+map.addControl(new maplibregl.NavigationControl(), 'bottom-right');
+map.addControl(new maplibregl.ScaleControl(), 'bottom-left');
+//map.addControl(new maplibregl.GeolocateControl(
+//  {positionOptions: {enableHighAccuracy: true}, trackUserLocation: true}), 'top-right');
+
+// 表示オプション
+map.showTileBoundaries = true;
+// map.showCollisionBoxes = true;
 map.on('load', function () {
-    map.addSource('tokyo_bus', {
-        type: 'geojson',
-        data: './kamedahoncho.geojson',
+    /*     map.addSource('area', {
+            type: 'geojson',
+            data: './okawa.geojson',
+        });
+        map.addLayer({
+            id: 'area',
+            type: 'fill',
+            source: 'area',
+            paint:
+                { 'fill-opacity': 0.2 },
+    
+        });
+        map.on('click', 'area', (e) => {
+            new maplibregl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML(e.features[0].properties.地番)
+                .addTo(map);
+        }); */
+    map.addSource('vector', {
+        type: 'vector',
+        tiles: ['./out/{z}/{x}/{y}.pbf'],
     });
     map.addLayer({
-        id: 'tokyo_bus',
+        id: 'vector',
         type: 'fill',
-        source: 'tokyo_bus',
+        source: 'vector',
+        'source-layer': 'vector',
         paint:
-        {'fill-opacity': 0.2},
+        { 'fill-opacity': 0.2 },
 
     });
 });
